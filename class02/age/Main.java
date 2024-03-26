0e072b2d1ab61f9892425565d7e2b535e7cacc69
Lucas Lopes <lucas.blopes3@gmail.com>
2024-03-26 07:30:52 -0300

    class02/age

--- class02/age/Main.java
+++ class02/age/Main.java
@@ -11,26 +11,26 @@ public class Main {
 
         System.out.println("Enter 5 age's: ");
         for(int i = 0; i < 5; i++) {
-            users[i] = input.nextInt();
-            avrg += users[i];    
+            users[i].setAge(input.nextInt());
+            avrg += users[i].getAge();
         }
 
         System.out.println("Average: " + avrg);
 
         System.out.println("Below avrg: ");
         for(int i = 0; i < 5; i++) {
-            if (users[i] < avrg) {
-                System.out.println(users[i] + ", ");
+            if (users[i].getAge() < avrg) {
+                System.out.println(users[i].getAge() + ", ");
             }
         }
 
         System.out.println("Above avrg: ");
         for(int i = 0; i < 5; i++) {
-            if (users[i] > avrg) {
-                System.out.println(users[i] + ", ");
+            if (users[i].getAge() > avrg) {
+                System.out.println(users[i].getAge() + ", ");
             }
         }
 
         input.close();
     }
-}
+i}
--- class02/age/Person.java
+++ class02/age/Person.java
@@ -2,4 +2,20 @@
 public class Person {
         
     private int age;
+
+    public Person() {
+        
+    }
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    } 
+
+    public int getAge() {
+        return age; 
+    }
 }
